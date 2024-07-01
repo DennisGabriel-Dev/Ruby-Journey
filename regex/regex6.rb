dd6895c35c6e5c100a81b77904a411a6da4ace0a
DennisGabriel <dennisgabr745@gmail.com>
2024-06-30 22:29:26 -0300

    add new anchors to regex

--- regex/regex6.rb
+++ regex/regex6.rb
@@ -14,3 +14,12 @@ puts show_regex 'Fats Waller', /z/ # no match
 puts show_regex 'yes | no', /\|/ # yes ->|<-no
 puts show_regex 'yes (no)', /\(no\)/ # yes ->(no)<-
 puts show_regex 'are you sure?', /e\?/ # are you sur->e?<-
+
+puts '-------- ANCHORS ---------'
+str = "this is\nthe time"
+puts show_regex(str, /^the/) # this is\n->the<- time
+puts show_regex(str, /is$/) # this ->is<-\nthe time
+puts show_regex(str, /\Athis/) # ->this<- is\nthe time
+puts show_regex(str, /\Athe/) # no match
+puts show_regex('theater', /^the$/) # no match
+puts show_regex('theater', /^the/) # ->the<-ather
