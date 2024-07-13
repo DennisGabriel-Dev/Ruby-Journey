539b2d1237969d82d0be659985f6bda9b5c5b3b5
DennisGabriel <dennisgabr745@gmail.com>
2024-07-13 14:00:21 -0300

    add more examples regex

--- regex/regex7.rb
+++ regex/regex7.rb
@@ -6,3 +6,10 @@ puts "six o'clock".gsub(/\B/, "*") # s*i*x o'c*l*o*c*k
 
 puts show_regex("this is\nthe time", /\bis/) # this ->is<-\nthe time
 puts show_regex("this is\nthe time", /\Bis/) # th->is<- is\nthe time
+
+a = "The moon is made of cheese"
+puts show_regex(a, /\w+/) # ->The<- moon is made of cheese
+puts show_regex(a, /\s.*\s/) #  The-> moon is made of <-cheese
+puts show_regex(a, /\s.*?\s/) #  The-> moon <-is made of cheese
+puts show_regex(a, /[aeiou]{2,99}/) #  The m->oo<-n is made of cheese
+puts show_regex(a, /mo??o/) #  The ->mo<-on is made of cheese
