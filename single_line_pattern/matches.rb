38f1e1502dadeeaa6068e8680a66a04ec26162e7
DennisGabriel <dennisgabr745@gmail.com>
2024-12-05 09:02:56 -0300

    add more examples to single line pattern

--- single_line_pattern/matches.rb
+++ single_line_pattern/matches.rb
@@ -14,5 +14,17 @@ puts(([1, 2, 3] in [Integer, Integer, Integer])) # true
 puts(([1, 2, 3] in [])) # false
 
 puts(({name: "Dennis", city: "Coelho Neto"} in {city: String})) # true
-
 puts(({name: "Dennis", city: "Coelho Neto"} in {})) # false
+
+puts ([3, "banana", "apple"] in [1..10, String, /p{2}/]) # true
+
+
+puts ([3, "banana", "apple"] in [1..10, String, *]) # true
+puts ([3, "banana", "apple"] in [*, String, *]) # true
+puts ({likes: [3, 5], dislikes: [2, 4]} in {likes: [1..5, *], dislikes: [2, *]}) # true
+
+puts ([1, 2, 3] in [Integer, Integer, Integer] | [String, String, String]) # true
+
+puts (["a", "b", "c"] in [Integer, Integer, Integer] | [String, String, String]) # true
+
+puts (["a", "b", :c] in [Integer, Integer, Integer] | [String, String, String]) # false
